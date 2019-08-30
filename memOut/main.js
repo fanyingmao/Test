92ad291e36d85b3cbbb9f803000cc069ed4ed824
fanyingmao <646900491@qq.com>
2019-08-30 15:39:39 +0800

    ff

--- memOut/main.js
+++ memOut/main.js
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 const heapdump = require('heapdump');
-=======
-// forever start -c "node --inspect=172.31.0.1:8025" ./memOut/main.js
-// node --inspect=172.31.0.1:8025 ./memOut/main.js
 
->>>>>>> ff
 var theThing = null;
 var replaceThing = function () {
     var originalThing = theThing;
@@ -16,8 +11,4 @@ var replaceThing = function () {
         someMethod: function () { }
     };
 };
-<<<<<<< HEAD
-setInterval(replaceThing, 100);
-=======
 setInterval(replaceThing, 1000);
->>>>>>> ff
